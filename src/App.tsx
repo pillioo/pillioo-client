ddf4def0f0135ffe0fe6ed4327ee3faca97ebb19
yoonkong1208 <yoona120808@gmail.com>
2026-07-12 13:30:47 +0900

    wip: SafetyInbox draft before syncing with jimni3155's PR #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ProductEntrance from './pages/ProductEntrance'
+import SafetyInbox from './pages/SafetyInbox'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProductEntrance />} />
+        <Route path="/app" element={<SafetyInbox />} />
       </Routes>
     </BrowserRouter>
   )
